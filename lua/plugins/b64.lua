69b0e4bd91966d13ac9d4f4b67c94351c7b538d4
Kari Honkasalo <kari.honkasalo@gmail.com>
2024-02-04 14:40:07 +0200

    Switch to a different base64 plugin

---
+++ lua/plugins/b64.lua
@@ -0,0 +1,7 @@
+return {
+  "taybart/b64.nvim",
+  keys = {
+    { "<leader>me", "<cmd>B64Encode<cr>", desc = "Base64 Encode", mode = "v" },
+    { "<leader>md", "<cmd>B64Decode<cr>", desc = "Base64 Decode", mode = "v" },
+  }
+}
