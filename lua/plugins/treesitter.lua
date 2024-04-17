35e0aa5754b3b4883944e257ea8145addc2e8792
Kari Honkasalo <kari.honkasalo@ericsson.com>
2024-04-17 13:10:15 +0000

    Disable treesitter for bigger files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,5 +2,16 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     auto_install = true,
+    highlight = {
+      enable = true,
+      disable = function(_, buf)
+        local max_filesize = 1000 * 1024 -- 1 MB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          vim.notify("Tree sitter disabled")
+          return true
+        end
+      end,
+    }
   }
 }
