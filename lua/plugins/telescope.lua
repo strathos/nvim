458d036bab2f627b8ba7206712eb595e9bdb21e0
Kari Kramsi <kari.kramsi@ericsson.com>
2024-06-10 08:15:40 +0000

    Remove live_grep_args keybind over default

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-telescope/telescope-live-grep-args.nvim",
   keys = {
-    { "<leader>/", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Grep with args" },
     { "<leader>s/", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Grep with args" },
   },
   config = function()
