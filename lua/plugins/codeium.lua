71c9ed03e3e3f8d899b62f0021a76532cb925b0c
Kari Kramsi <kari.kramsi@gmail.com>
2025-01-18 17:59:07 +0200

    Updates and switch to Blink from nvim-cmp

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,19 +1,41 @@
 return {
-  "Exafunction/codeium.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      if os.getenv("WORK_ENV") == "true" then
+        require("codeium").setup({
+          api = {
+            host = os.getenv("CODEIUM_HOST")
+          },
+          enterprise_mode = true
+        })
+      else
+        require("codeium").setup({})
+      end
+    end,
   },
-  config = function()
-    if os.getenv("WORK_ENV") == "true" then
-      require("codeium").setup({
-        api = {
-          host = os.getenv("CODEIUM_HOST")
+  {
+    "saghen/blink.compat",
+    opts = {
+      enable_events = true
+    }
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        compat = { "codeium" },
+        providers = {
+          codeium = {
+            kind = "Codeium",
+            score_offset = 100,
+            async = true,
+          },
         },
-        enterprise_mode = true
-      })
-    else
-      require("codeium").setup({})
-    end
-  end,
+      },
+    },
+  }
 }
