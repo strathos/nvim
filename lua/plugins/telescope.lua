5014486cdf2adac1540402ad0c19d74e1bd1f8da
Kari Kramsi <kari.kramsi@ericsson.com>
2025-01-30 09:24:38 +0200

    Remove live_grep_args as LazyVim got rid of Telescope

--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-telescope/telescope-live-grep-args.nvim",
-  keys = {
-    { "<leader>s/", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Grep with args" },
-  },
-  config = function()
-    require("telescope").load_extension("live_grep_args")
-  end
-}
