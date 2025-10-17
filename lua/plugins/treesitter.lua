287858fa629f9e5fca8eb8c1bd2ae3c5189d63f8
Kari Kramsi <kari.kramsi@ericsson.com>
2025-10-17 12:43:32 +0000

    Disable lazy loading for TreeSitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
   opts = {
     auto_install = true,
     highlight = {
       enable = true,
       disable = function(_, buf)
-        local max_filesize = 1000 * 1024 -- 1 MB
+        local max_filesize = 2 * 1000 * 1024 -- 2 MB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
           vim.notify("Tree sitter disabled")
