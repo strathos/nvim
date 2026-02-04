ce4ee208b1b9cc8f8f48d0aaad5f9c32228c5c49
Kari Kramsi <kari.kramsi@ericsson.com>
2026-02-04 14:48:28 +0000

    Add Y as mapping in NeoTree to copy filename

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -34,6 +34,38 @@ return {
     window = {
       mappings = {
         ["<space>"] = "none",
+        -- https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370#discussioncomment-4144005
+        ['Y'] = function(state)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
+
+          local results = {
+            filepath,
+            modify(filepath, ':.'),
+            modify(filepath, ':~'),
+            filename,
+            modify(filename, ':r'),
+            modify(filename, ':e'),
+          }
+
+          vim.ui.select({
+            'Absolute path: ' .. results[1],
+            'Path relative to CWD: ' .. results[2],
+            'Path relative to HOME: ' .. results[3],
+            'Filename: ' .. results[4],
+            'Filename without extension: ' .. results[5],
+            'Extension of the filename: ' .. results[6],
+          }, { prompt = 'Choose to copy to clipboard:' }, function(_, idx)
+            if not idx then
+              return
+            end
+            local result = results[idx]
+            vim.fn.setreg('+', result)
+            vim.notify('Copied: ' .. result)
+          end)
+        end
       },
     },
     default_component_configs = {
